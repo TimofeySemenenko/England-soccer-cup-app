ee82160adfe7d9d9496d10d222433e4819c62b2e
tim_get_me <gtcntKM$%6>
2020-12-06 17:49:29 +0300

    second fix

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => Itrim($DATABASE_URL),
+            'url' => $DATABASE_URL,
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
