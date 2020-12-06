9999fa92d747a7456bda69489b9626744e76fe1c
tim_get_me <gtcntKM$%6>
2020-12-06 19:34:05 +0300

    registered seder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\EnglandCupSeeders\DivisionsSeeders;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(DivisionsSeeders::class);
     }
 }
